ee8e98891e2d5652bf6e78a0cfac0d11c337a921
Bogdan Popa <bogdan@defn.io>
2019-05-08 10:48:42 +0300

    fix(tests): omit cli.rkt from tests

--- north/info.rkt
+++ north/info.rkt
@@ -3,13 +3,18 @@
 (define version "0.1.0")
 (define collection "north")
 
-(define deps '("base"
-               "db-lib"
-               "gregor-lib"))
-(define build-deps '("at-exp-lib"
-                     "racket-doc"
-                     "rackunit-lib"
-                     "scribble-lib"))
+(define deps
+  '("base"
+    "db-lib"
+    "gregor-lib"))
+
+(define build-deps
+  '("at-exp-lib"
+    "racket-doc"
+    "rackunit-lib"
+    "scribble-lib"))
+
+(define test-omit-paths '("cli.rkt"))
 
 (define raco-commands '(("north" north/cli "run schema migrations" #f)))
 
