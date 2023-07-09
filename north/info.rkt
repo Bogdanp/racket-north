9b0499fa071a8e405a6976170f3c45b2eae9c1d8
Bogdan Popa <bogdan@defn.io>
2023-07-09 13:53:17 +0300

    doc: add to Databases category

--- north/info.rkt
+++ north/info.rkt
@@ -3,21 +3,19 @@
 (define license 'BSD-3-Clause)
 (define version "0.5")
 (define collection "north")
-
 (define deps
   '("base"
     "db-lib"
     "gregor-lib"
     "parser-tools-lib"))
-
 (define build-deps
   '("at-exp-lib"
     "racket-doc"
     "rackunit-lib"
     "scribble-lib"))
-
-(define test-omit-paths '("cli.rkt"))
-
-(define raco-commands '(("north" north/cli "run schema migrations" #f)))
-
-(define scribblings '(("north.scrbl")))
+(define test-omit-paths
+  '("cli.rkt"))
+(define raco-commands
+  '(("north" north/cli "run schema migrations" #f)))
+(define scribblings
+  '(("north.scrbl" () ("Databases"))))
